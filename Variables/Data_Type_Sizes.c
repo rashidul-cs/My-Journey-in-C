1c21b9ccebce1608be04956ab4fe00c4040f68e6
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-25 00:12:15 +0600

    Create Data_Type_Sizes.c

---
+++ Variables/Data_Type_Sizes.c
@@ -0,0 +1,9 @@
+
+// Chapter 1 (Variables) - Task 1.1: Data Type Sizes
+#include <stdio.h>
+int main(){
+    printf("The size of int is %zu bytes\n", sizeof(int));
+    printf("The size of float is %zu bytes\n", sizeof(float));
+    printf("The size of char is %zu byte\n", sizeof(char));
+    return 0;
+}
