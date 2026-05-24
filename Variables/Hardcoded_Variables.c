26e1bfdf3562f2b0fd176e79334dd705e4ea45fd
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-25 00:24:43 +0600

    Create Hardcoded_Variables.c

---
+++ Variables/Hardcoded_Variables.c
@@ -0,0 +1,14 @@
+
+// Chapter 1 (Variables) - Task 2.2: Hardcoded Variables
+#include <stdio.h>
+int main(){
+    int a = 6;
+    float b = 75.3;
+    char c = 'g', d[10] = "Rifat";
+
+    printf("a is %d\n", a);
+    printf("b is %.2f\n", b);
+    printf("c is %c\n", c);
+    printf("d is %s\n", d);
+    return 0;
+}
