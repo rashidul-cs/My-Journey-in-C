4e37a3862c38c6da87ed0208d8afd87ecd73788e
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-26 00:13:07 +0600

    Create 2.1_Implicit_Conversion.c

---
+++ Instructions_and_Operators/2.1_Implicit_Conversion.c
@@ -0,0 +1,9 @@
+
+// Chapter 2 (Instructions & Operators) - Task 2.1: Implicit Conversion Variables
+#include <stdio.h>
+int main(){
+    int a = 9.54;
+    float b = 6;
+    printf("a is %d and b is %.2f\n", a, b);
+    return 0;
+}
