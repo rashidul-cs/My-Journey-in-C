33ed9134edb589cf86c99fc5fdd688d04fb34a60
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-26 00:11:45 +0600

    Create 1_Arithmetic_Operators.c

---
+++ Instructions_and_Operators/1_Arithmetic_Operators.c
@@ -0,0 +1,10 @@
+
+// Chapter 2 (Instructions & Operators) - Task 1: Arithmetic Operators
+#include <stdio.h>
+int main(){
+    int a = 45, b = 35, c;
+    c = a+b;
+    printf(" The value of a is %d\n The value of b is %d\n The value of a+b is %d\n", a, b, a+b);
+    printf("The remainder when a is divide by b is %d\n", a%b);
+    return 0;
+}
