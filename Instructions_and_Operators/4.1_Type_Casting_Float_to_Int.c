1393411e525763c8bc56aeebe0e5ac539265099d
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-26 00:16:13 +0600

    Create 4.1_Type_Casting_Float_to_Int.c

---
+++ Instructions_and_Operators/4.1_Type_Casting_Float_to_Int.c
@@ -0,0 +1,10 @@
+
+// Chapter 2 (Instructions & Operators) - Task 4.1: Type Casting Float to Int
+#include <stdio.h>
+int main(){
+    int a = 55;
+    float b = 34.64;
+    int c = (int) b;
+    printf("c is %d\n", c);
+    return 0;
+}
