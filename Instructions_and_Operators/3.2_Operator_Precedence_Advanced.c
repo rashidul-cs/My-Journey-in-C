1317966e646b2a93ddf4e73c968c3c028cc92ef3
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-26 00:15:35 +0600

    Create 3.2_Operator_Precedence_Advanced.c

---
+++ Instructions_and_Operators/3.2_Operator_Precedence_Advanced.c
@@ -0,0 +1,8 @@
+
+// Chapter 2 (Instructions & Operators) - Task 3.2: Operator Precedence Complex Expression
+#include <stdio.h>
+int main(){
+    int a = 3, b =  6, c = 9;
+    printf("The value is %d\n", 3*b/2*c + 7*a);
+    return 0;
+}
