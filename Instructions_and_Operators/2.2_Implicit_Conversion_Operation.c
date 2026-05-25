7d2c3a478ecd15e21d524c71f756181af8b146c6
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-26 00:14:00 +0600

    Create 2.2_Implicit_Conversion_Operation.c

---
+++ Instructions_and_Operators/2.2_Implicit_Conversion_Operation.c
@@ -0,0 +1,9 @@
+
+// Chapter 2 (Instructions & Operators) - Task 2.2: Implicit Conversion in Operation
+#include <stdio.h>
+int main(){
+    int a = 9.54;
+    float b = 6;
+    printf("The value of a/b is %.2f\n", a/b);
+    return 0;
+}
