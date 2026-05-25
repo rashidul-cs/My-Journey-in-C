6a9732751dee245204f6cb592e7fbd7cd9ab80f4
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-26 00:14:44 +0600

    Create 3.1_Operator_Precedence.c

---
+++ Instructions_and_Operators/3.1_Operator_Precedence.c
@@ -0,0 +1,8 @@
+
+// Chapter 2 (Instructions & Operators) - Task 3.1: Operator Precedence Basic
+#include <stdio.h>
+int main(){
+    int a = 3, b =  6, c = 9;
+    printf("The value is %d\n", a*b/c +7);
+    return 0;
+}
