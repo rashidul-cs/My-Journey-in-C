823274508db28cadca51ba8316be8a2eb764407f
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-27 00:16:56 +0600

    Create 3_Logical_Operators.c

---
+++ Conditional_Instructions/3_Logical_Operators.c
@@ -0,0 +1,11 @@
+
+// Chapter 3 (Conditional Instructions) - Task 3: Logical Operators
+#include <stdio.h>
+int main(){
+    int a = 1, b = 0;
+    printf("The value of a and b is %d\n", a&&b);
+    printf("The value of a or b is %d\n", a||b);
+    printf("The value of !a is %d\n", !a);
+    printf("The value of !b is %d\n", !b);
+    return 0;
+}
