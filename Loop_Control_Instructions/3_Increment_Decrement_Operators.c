27539f50b62c62273b6f8b2ce0e563ea902b4018
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-31 23:33:01 +0600

    Create 3_Increment_Decrement_Operators.c

---
+++ Loop_Control_Instructions/3_Increment_Decrement_Operators.c
@@ -0,0 +1,14 @@
+
+// Chapter 4 (Loop Control Instructions) - Task 3: Pre and Post Increment/Decrement
+#include <stdio.h>
+int main(){
+    int i = 5;
+    printf("i is %d\n", i);
+    i = i+5;
+    printf("i is %d\n", i);
+    printf("i is %d\n", i++);
+    printf("i is %d\n", ++i);
+    printf("i is %d\n", i--);
+    printf("i is %d\n", i);
+    return 0;
+}
