b0c8b89bafdf2c62efd3148beedd3c52c3423ad1
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-31 23:42:17 +0600

    Create 8_For_Loop_Decrement.c

---
+++ Loop_Control_Instructions/8_For_Loop_Decrement.c
@@ -0,0 +1,9 @@
+
+// Chapter 4 (Loop Control Instructions) - Task 8: Decrementing For Loop
+#include <stdio.h>
+int main(){
+    for (int i = 34; i>0; i--){
+        printf("%d\t", i);
+    }
+    return 0;
+}
