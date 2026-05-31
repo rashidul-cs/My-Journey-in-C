29cb9b7ce2a3476303e0d76d28f6a88a118de400
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-31 23:31:07 +0600

    Create 1.2_While_Loop_Text_Print.c

---
+++ Loop_Control_Instructions/1.2_While_Loop_Text_Print.c
@@ -0,0 +1,11 @@
+
+// Chapter 4 (Loop Control Instructions) - Task 1.2: While Loop Text Printing
+#include <stdio.h>
+int main(){
+    int i = 0;
+    while (i < 4){
+        printf("happy birthday\n");
+        i++;
+    }
+    return 0;
+}
