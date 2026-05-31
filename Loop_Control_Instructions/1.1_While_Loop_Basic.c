50773f77909eb379bc7c2dd57e7e56fa5ef6b45e
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-31 23:29:42 +0600

    Create 1.1_While_Loop_Basic.c

---
+++ Loop_Control_Instructions/1.1_While_Loop_Basic.c
@@ -0,0 +1,11 @@
+
+// Chapter 4 (Loop Control Instructions) - Task 1.1: While Loop Basic
+#include <stdio.h>
+int main(){
+    int i = 0;
+    while (i < 10){
+        printf("i is %d\n", i);
+        i++;
+    }
+    return 0;
+}
