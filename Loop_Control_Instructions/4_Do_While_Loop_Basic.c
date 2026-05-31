a32384dc40638a4e8dfe775b57743a76248f4c58
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-31 23:33:51 +0600

    Create 4_Do_While_Loop_Basic.c

---
+++ Loop_Control_Instructions/4_Do_While_Loop_Basic.c
@@ -0,0 +1,11 @@
+
+// Chapter 4 (Loop Control Instructions) - Task 4: Do While Loop Basic
+#include <stdio.h>
+int main(){
+    int i = 0;
+    do {
+        printf("i is %d\n", i);
+        i++;
+    } while (i <= 10);
+    return 0;
+}
