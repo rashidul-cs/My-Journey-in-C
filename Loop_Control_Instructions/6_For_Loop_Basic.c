fb4ef28bc98fb00bb826b1a8cc0840a7ff772bec
Md Rashidul Islam <rashid6788g@gmail.com>
2026-05-31 23:36:22 +0600

    Create 6_For_Loop_Basic.c

---
+++ Loop_Control_Instructions/6_For_Loop_Basic.c
@@ -0,0 +1,9 @@
+
+// Chapter 4 (Loop Control Instructions) - Task 6: For Loop Basic
+#include <stdio.h>
+int main(){
+    for (int i = 0; i<=10; i++){
+        printf("i is %d\n", i);
+    }
+    return 0;
+}
