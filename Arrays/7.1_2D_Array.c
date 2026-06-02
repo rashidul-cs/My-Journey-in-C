880c4c22188478919dbd38452cc20e5a0bbb5b72
Md Rashidul Islam <rashid6788g@gmail.com>
2026-06-03 01:04:46 +0600

    Create 7.1_2D_Array.c

---
+++ Arrays/7.1_2D_Array.c
@@ -0,0 +1,21 @@
+
+// Chapter 7 (Array) - Task 7.1 2d array
+#include <stdio.h>
+int main(){
+    int arr [3][2];
+
+    for (int i = 0; i<3; i++){
+        for (int j = 0; j<2; j++){
+            printf("Enter the value of arr[%d][%d]: ", i, j);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    for (int i = 0; i<3; i++){
+        for (int j = 0; j<2; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
