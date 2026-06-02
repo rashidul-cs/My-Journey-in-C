f421befc03aada1cb5b82f070a27b787640cd02e
Md Rashidul Islam <rashid6788g@gmail.com>
2026-06-03 00:49:03 +0600

    Create 1_Array_Basics.c

---
+++ Arrays/1_Array_Basics.c
@@ -0,0 +1,10 @@
+
+// Chapter 7 (Arrays) - Task 1: Introduction to Arrays and Element Assignment
+#include <stdio.h>
+int main(){
+    int marks[90];
+    marks[0] = 56;
+    marks[1] = 33;
+    printf(" Marks 0 is %d\n Marks 1 is %d\n", marks[0], marks[1]);
+    return 0;
+}
