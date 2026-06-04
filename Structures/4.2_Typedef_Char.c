044aa9fd350f84a6edca361a96a578ec253a8962
Md Rashidul Islam <rashid6788g@gmail.com>
2026-06-05 02:15:27 +0600

    Create 4.2_Typedef_Char.c

---
+++ Structures/4.2_Typedef_Char.c
@@ -0,0 +1,9 @@
+
+// Chapter 9: Structures - Task 4.2 (by me using typedef with char)
+#include <stdio.h>
+int main(){
+    typedef char harry;
+    harry a = 'g';
+    printf("The value of a is %c\n", a);
+    return 0;
+}
