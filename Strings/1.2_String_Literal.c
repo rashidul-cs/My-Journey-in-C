35824f43b9ca8ab2088fa355d10f5718597cf252
Md Rashidul Islam <rashid6788g@gmail.com>
2026-06-04 20:26:35 +0600

    Create 1.2_String_Literal.c

---
+++ Strings/1.2_String_Literal.c
@@ -0,0 +1,10 @@
+
+// Chapter 8: String - Task 1.2
+#include <stdio.h>
+int main(){
+    char st[] = {"abc"};
+    for(int i = 0; i<3; i++){
+        printf("Character: %c\n", st[i]);
+    }
+    return 0;
+}
