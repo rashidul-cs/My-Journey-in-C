edd28bafe9cb283cf9e156262a37b785e58a5396
Md Rashidul Islam <rashid6788g@gmail.com>
2026-06-04 20:45:30 +0600

    Create 8.1_String_Strcmp_1.c

---
+++ Strings/8.1_String_Strcmp_1.c
@@ -0,0 +1,9 @@
+
+// Chapter 8: String - Task 8.1 (strcmp) (two string comparison)
+#include <stdio.h>
+#include <string.h>
+int main(){
+    int a = strcmp("deep", "joke");
+    printf("%d", a);
+    return 0;
+}
