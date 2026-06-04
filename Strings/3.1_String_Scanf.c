2549d7f8cf53deadfe43bdf4e0d6908f101dcfa6
Md Rashidul Islam <rashid6788g@gmail.com>
2026-06-04 20:36:16 +0600

    Create 3.1_String_Scanf.c

---
+++ Strings/3.1_String_Scanf.c
@@ -0,0 +1,10 @@
+
+// Chapter 8: String - Task 3.1
+#include <stdio.h>
+int main(){
+    char st[7];
+    printf("Enter a string: ");
+    scanf("%s", st);
+    printf("You Entered String: %s\n", st);
+    return 0;
+}
