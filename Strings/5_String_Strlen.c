f446dde062bfec5c6b89eee0726d14162e6d8cf0
Md Rashidul Islam <rashid6788g@gmail.com>
2026-06-04 20:42:10 +0600

    Create 5_String_Strlen.c

---
+++ Strings/5_String_Strlen.c
@@ -0,0 +1,9 @@
+
+// Chapter 8: String - Task 5 using string fuction
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char st[] = "harry";
+    printf("%d\n", strlen(st));
+    return 0;
+}
