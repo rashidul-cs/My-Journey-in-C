9e0cd4b95f8113b8b59857e2ea7602ee489bcd74
Md Rashidul Islam <rashid6788g@gmail.com>
2026-06-05 02:14:36 +0600

    Create 4.1_Typedef_Int.c

---
+++ Structures/4.1_Typedef_Int.c
@@ -0,0 +1,9 @@
+
+// Chapter 9: Structures - Task 4.1 (basic typedef with int)
+#include <stdio.h>
+int main(){
+    typedef int harry;
+    harry a = 66;
+    printf("The value of a is %d\n", a);
+    return 0;
+}
