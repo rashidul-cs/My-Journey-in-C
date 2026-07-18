818536c09f7de646118bf9d908f522daa96c2cd0
Md Rashidul Islam <rashid6788g@gmail.com>
2026-07-18 21:21:19 +0600

    Create bitwise_and_or.c

---
+++ By_me/bitwise_and_or.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+int main(){
+    int a = 1, b = 0;
+    printf("value of a and b: %d\n", a&b);
+    printf("value of a or b: %d\n", a|b);
+    printf("value of not a: %d\n", !a);
+    printf("value of not b: %d\n", !b);
+    return 0;
+}
